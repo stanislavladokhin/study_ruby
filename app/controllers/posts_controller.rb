7d10729595a530af338db62257a863cc15f886c0
Name <stasladokhin@hotmail.com>
2014-11-14 18:48:01 +0300

    new forms for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,10 +10,25 @@ class PostsController < ApplicationController
   def show
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    puts params
+    Post.create(post_params)
+    #flash[:notice] = "Post was succesfully created"
+    redirect_to posts_path, notice: "Post was succesfully created"
+  end
+
   private
     def post
       @post ||= Post.find( params[:id] )
     end
+
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end 
 
     helper_method :post
 end
