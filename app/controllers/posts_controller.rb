284ed23665df197472d9ad4210f73f5a74f07640
Name <stasladokhin@hotmail.com>
2014-11-26 23:09:44 +0300

    Add Edit and Delete function for posts in app. Create new view(edit.html.erb). Updates in routes.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-
+before_filter :post, only: [:show, :edit, :update, :destroy]
 
   def index
     # instance param
@@ -20,6 +20,26 @@ class PostsController < ApplicationController
     #flash[:notice] = "Post was succesfully created"
     redirect_to posts_path, notice: "Post was succesfully created"
   end
+
+  def edit 
+  end
+
+  def update
+    @post.assign_attributes(post_params)
+    if @post.save
+      flash[:notice] = "Post was succesfully updated"
+    else
+      flash[:notice] = "Post was not succesfully updated"
+    end
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post.destroy
+      flash[:notice] = "Post was succesfully deleted"
+    redirect_to posts_path
+  end
+
 
   private
     def post
